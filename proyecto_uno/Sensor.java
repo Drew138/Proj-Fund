32f8814f9d18a8a9f42bc50617014e321c3fd7ae
mmc31-08 <mateo-31-cadavid@outlook.com>
2020-09-28 15:47:20 -0500

    avance 1 de clases

---
+++ proyecto_uno/Sensor.java
@@ -0,0 +1,21 @@
+public class Sensor{
+    public static Sensor[];
+    private int estado;
+
+    public Sensor (int e){
+
+    }
+
+    public String toString(){
+
+    }
+
+    public static String sensorLibre(){
+
+    }
+
+    public static String sensoresEstado(){
+
+    }
+}
+
---
+++ proyecto_uno/Vehiculo.java
@@ -0,0 +1,33 @@
+public class Vehiculo{
+    public static Vehiculo[];
+    public static int cantidad;
+    private String placa;
+    private String marca;
+    private String color;
+    private int valorComercial;
+
+    public Vehiculo(){
+
+    }
+
+    public Vehiculo(String p, String m, String c){
+
+    }
+
+    public Vehiculo(String p, String m, String c, int v){
+
+    }
+
+    public String toString(){
+
+    }
+
+    public static String toStringVehiculos(){
+
+    }
+
+    public int cantidadVehiculos(){
+
+    }
+
+}
